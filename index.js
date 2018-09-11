b586471e46c23a75b8e5b033ab1f2b0a6fb399d8
Jason To <jasto@ucdavis.edu>
2018-09-11 05:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ function johnLennonFacts(John_facts){
   return empty_array;
 }
 
-const facts = ["He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts)
 { 
