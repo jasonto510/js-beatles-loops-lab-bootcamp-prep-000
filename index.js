9b05e7932a181e3adfd6fc96f9aa29f0eddfc481
Jason To <jasto@ucdavis.edu>
2018-09-11 05:25:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 
-const facts = ["He was the last Beatle to learn to drive",
+const John_facts = ["He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"];
