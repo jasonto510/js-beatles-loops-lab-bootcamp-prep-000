2a8cc3f799b9f66fbe9f6cac538f4bb62c7d89cc
Jason To <jasto@ucdavis.edu>
2018-09-11 05:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+
+function theBeatlesPlay(array_musicians, array_instruments) {
+  var empty_array = [];
+  var array_length = array_musicians.length;
+  for (var i = 0; i < array_length; i ++){ 
+  empty_array.push(array_musicians[i] + " plays " +  array_instruments[i]);
+  }
+      return empty_array;
+}
+
+function johnLennonFacts(John_facts){
+  var array_length = John_facts.length;
+  var empty_array = [];
+  var i = 0;
+  while (array_length > 0){
+  empty_array.push(John_facts[i] + "!!!");
+  i ++;
+  }
+  return empty_array;
+}
+
+
